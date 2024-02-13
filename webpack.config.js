da9bd4f87bbd4dfd0a9fa770a96465bf729f5555
givisab <givisab12345@gmail.com>
2024-02-13 11:44:36 +0400

    Add Babel to webpack config

--- webpack.config.js
+++ webpack.config.js
@@ -6,5 +6,22 @@ module.exports = {
     filename: 'main.js',
     path: path.resolve(__dirname, 'dist'),
   },
+  module: {
+  rules: [
+    {
+      test: /\.(?:js|mjs|cjs)$/,
+      exclude: /node_modules/,
+      use: {
+        loader: 'babel-loader',
+        options: {
+          presets: [
+            ['@babel/preset-env', { targets: "defaults" }]
+          ],
+          plugins: ['@babel/plugin-proposal-class-properties']
+          }
+        }
+      }
+    ]
+  },
   mode: 'development'
 };
